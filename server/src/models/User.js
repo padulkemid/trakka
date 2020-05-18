7346e33b7fb50db68724601097f62893a7264353
muhammadKholid <muhamma4dkhal3d@gmail.com>
2020-05-18 17:55:42 +0700

    reprot this time: 1. add user models 2. installing bcryptjs npm

---
+++ server/src/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: [6, 'too few charachters'],
+  },
+});
+
+userSchema.pre('save', (next) => {
+  if (!this.isModified('password')) {
+    next();
+  } else {
+    bcrypt
+      .genSalt(5)
+      .then((salt) => bcrypt.hash(this.password, salt))
+      .then((hash) => {
+        this.password = hash;
+        next();
+      })
+      .catch(next);
+  }
+});
+
+userSchema.method = {
+  comparePassword: (candidatePassword) => {
+    const compared = bcrypt.compare(candidatePassword, this.password);
+    return compared;
+  },
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
