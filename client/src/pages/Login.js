0e46ca063b1f7d6d990c94ca7065341cc316353b
Fadhil Muhammad <bringitdown0@gmail.com>
2020-05-19 14:10:43 +0700

    Summary of this commit:   - Add login state   - handle email using localStorage

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,44 +1,52 @@
-import React from 'react'
-import Form from '../components/FormLogin&Register'
-import gql from 'graphql-tag'
-import {useMutation} from '@apollo/react-hooks'
+import React from 'react';
+import Form from '../components/FormLogin&Register';
+import gql from 'graphql-tag';
+import { useHistory } from 'react-router-dom';
+import { useMutation } from '@apollo/react-hooks';
 
-const LOGIN = gql `
-    mutation loginUser($input: UserLogin!){
-        login(input: $input){
-            result
-        }
+const LOGIN = gql`
+  mutation loginUser($input: UserLogin!) {
+    login(input: $input) {
+      result
     }
-`
+  }
+`;
 
 export default () => {
+  const [loginUser] = useMutation(LOGIN);
+  const history = useHistory();
 
-    const [loginUser] = useMutation(LOGIN)
-    
-    async function Login(data) {
-        console.log(data)
-        try {
-            const result = await loginUser({
-                variables: {
-                    input: {
-                        email: data.email,
-                        password: data.password
-                    }
-                }
-            })
-            console.log(result.data.login.result)
-        } catch (error) {
-            console.log(error)
-        }
+  async function Login(data) {
+    try {
+      const result = await loginUser({
+        variables: {
+          input: {
+            email: data.email,
+            password: data.password,
+          },
+        },
+      });
+      const reportedResult = result.data.login.result;
+      if (!reportedResult.length) {
+        console.log('Something happened, your password is wrong / something');
+      } else {
+        localStorage.setItem('email', data.email);
+        history.push('/dashboard');
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    return (
-        <div className='container'>
-            <center>
-                <h1> Login </h1>
-            </center>
-            <br/>
-            <Form action={Login} status='login' />
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <center>
+        <h1> Login </h1>
+      </center>
+      <br />
+      <Form action={Login} status="login" />
+    </div>
+  );
+};
+
