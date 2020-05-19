c138985c51bbdd43e6dcf88dbaf7d6aa213bae05
Fadhil Muhammad <bringitdown0@gmail.com>
2020-05-19 16:22:54 +0700

    Add opacity differences for passed/unpassed/unattended events GraphQl queries is now working on edit

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,43 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import {
-    useHistory
-} from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
 export default ({ event, date }) => {
-    const history = useHistory();
-    const [formattedTime, setFormattedTime] = useState('00:00');
-    const [isPassed, setIsPassed] = useState(false);
+  const history = useHistory();
+  const [formattedTime, setFormattedTime] = useState('00:00');
+  const [isPassed, setIsPassed] = useState(false);
 
-    useEffect(() => {
-        let eventDate = new Date();
-        if (event.timestamp) {
-            eventDate = new Date(event.timestamp);
-            setFormattedTime(getFormattedTime(eventDate));
-            setIsPassed(false);
-            if (date) {
-                if (date > eventDate) {
-                    setIsPassed(true);
-                }
-            }
+  useEffect(() => {
+    let eventDate = new Date();
+    if (event.timestamp) {
+      eventDate = new Date(event.timestamp);
+      setFormattedTime(getFormattedTime(eventDate));
+      setIsPassed(false);
+      if (date) {
+        if (date > eventDate) {
+          setIsPassed(true);
         }
-    }, [event, date]);
+      }
+    }
+  }, [event, date]);
 
-    const getFormattedTime = (date) => {
-        const hour = date.getHours();
-        const minute = date.getMinutes();
-        return `${hour > 12 ? hour - 12 : hour}:${
-            minute > 9 ? minute : `0${minute}`
-            } ${hour > 12 ? 'pm' : 'am'}`;
-    };
-    return (
-        <div className={`card${isPassed ? 'passed' : ''}`} onClick={(e) => { if (event) history.push(`/dashboard/edit/${event.id}`) }}>
-            <h4 className="title">{event.title}</h4>
-            <h5 className="date">{formattedTime}</h5>
-            <p className="desc">{event.description}</p>
-            <div className="edit transition">
-                <h3>Edit</h3>
-            </div>
-        </div>
-    );
+  const getFormattedTime = (date) => {
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    return `${hour > 12 ? hour - 12 : hour}:${
+      minute > 9 ? minute : `0${minute}`
+    } ${hour > 12 ? 'pm' : 'am'}`;
+  };
+  return (
+    <div
+      className={`card ${isPassed ? 'passed' : ''}`}
+      onClick={(e) => {
+        if (event) history.push(`/dashboard/edit/${event.id}`);
+      }}>
+      <h4 className="title">{event.title}</h4>
+      <h5 className="date">{formattedTime}</h5>
+      <p className="desc">{event.description}</p>
+      <div className="edit transition">
+        <h3>Edit</h3>
+      </div>
+    </div>
+  );
 };
-
