3ce8109f51364f509f7f47e4296d94369d4624bc
Alan Budiarto <alanwbudiarto@gmail.com>
2020-05-19 12:22:01 +0700

    Fix Add Edit

--- client/src/pages/AddEvent.js
+++ client/src/pages/AddEvent.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { CreateEvent } from '../actions';
+import { CREATE_EVENT, GET_ALL_EVENTS } from "../schema";
+import { useMutation } from '@apollo/react-hooks';
 import { FormEditAdd } from '../components';
 
 export default () => {
+
+    const [createEvent] = useMutation(CREATE_EVENT, { refetchQueries: [{ query: GET_ALL_EVENTS }] });
+    const CreateEvent = async (event) => {
+        try {
+            const result = await createEvent({
+                variables: {
+                    event
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className='container'>
             <center>
--- client/src/pages/EditEvent.js
+++ client/src/pages/EditEvent.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { EditEventById } from '../actions';
+import { EDIT_EVENT, GET_ALL_EVENTS } from "../schema";
+import { useMutation } from '@apollo/react-hooks';
 import { FormEditAdd } from '../components';
 
 export default () => {
+
+    const [editEvent] = useMutation(EDIT_EVENT, { refetchQueries: [{ query: GET_ALL_EVENTS }] });
+    const EditEventById = async (id, event) => {
+        try {
+            const result = await editEvent({
+                variables: {
+                    id,
+                    event
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className='container'>
             <center>
