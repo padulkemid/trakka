112c9bf37913e5f4ce253ab90cd0c0402f56471b
Alan Budiarto <alanwbudiarto@gmail.com>
2020-05-19 15:14:33 +0700

    Dashboard Add Edit Btn

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,29 +1,35 @@
 import React, { useState, useEffect } from 'react';
+import {
+    useHistory
+} from "react-router-dom";
 
 export default ({ event }) => {
-  const [formattedTime, setFormattedTime] = useState('00:00');
+    const history = useHistory();
+    const [formattedTime, setFormattedTime] = useState('00:00');
 
-  useEffect(() => {
-    if (event.timestamp) {
-      const convertDate = Number(event.timestamp);
-      setFormattedTime(getFormattedTime(new Date(convertDate)));
-    }
-  }, [event]);
+    useEffect(() => {
+        if (event.timestamp) {
+            setFormattedTime(getFormattedTime(new Date(event.timestamp)));
+        }
+    }, [event]);
 
-  const getFormattedTime = (date) => {
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    return `${hour > 12 ? hour - 12 : hour}:${
-      minute > 9 ? minute : `0${minute}`
-    } ${hour > 12 ? 'pm' : 'am'}`;
-  };
+    const getFormattedTime = (date) => {
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+        return `${hour > 12 ? hour - 12 : hour}:${
+            minute > 9 ? minute : `0${minute}`
+            } ${hour > 12 ? 'pm' : 'am'}`;
+    };
 
-  return (
-    <div className="card">
-      <h4 className="title">{event.title}</h4>
-      <h5 className="date">{formattedTime}</h5>
-      <p className="desc">{event.description}</p>
-    </div>
-  );
+    return (
+        <div className="card" onClick={(e) => { if (event) history.push(`/dashboard/edit/${event.id}`) }}>
+            <h4 className="title">{event.title}</h4>
+            <h5 className="date">{formattedTime}</h5>
+            <p className="desc">{event.description}</p>
+            <div className="edit transition">
+                <h3>Edit</h3>
+            </div>
+        </div>
+    );
 };
 
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { GET_ALL_EVENTS } from '../schema';
 import { useQuery } from '@apollo/react-hooks';
 import { CardList } from '../components';
+import {
+  useHistory
+} from "react-router-dom";
 
 export default () => {
+  const history = useHistory();
   const { loading, error, data } = useQuery(GET_ALL_EVENTS);
   const [curDate, setCurDate] = useState('');
 
@@ -14,11 +18,28 @@ export default () => {
     setCurDate(dateArray.slice(0, 3).join(' '));
   }, []);
 
+  const testData = {
+    getEvents: [
+      { id: 0, title: "Test", description: "Lorem Ipsum is simply dummy text of the printing a…ldus PageMaker including versions of Lorem Ipsum.", timestamp: "2020-05-19T07:25:44.922Z" },
+      { id: 0, title: "Test", description: "Lorem Ipsum is simply dummy text of the printing a…ldus PageMaker including versions of Lorem Ipsum.", timestamp: "2020-05-19T07:25:44.922Z" },
+      { id: 0, title: "Test", description: "Lorem Ipsum is simply dummy text of the printing a…ldus PageMaker including versions of Lorem Ipsum.", timestamp: "2020-05-19T07:25:44.922Z" },
+      { id: 0, title: "Test", description: "Lorem Ipsum is simply dummy text of the printing a…ldus PageMaker including versions of Lorem Ipsum.", timestamp: "2020-05-19T07:25:44.922Z" },
+    ]
+  }
+
   return (
-    <div className="container">
-      <h1>{curDate}</h1>
-      {data ? <CardList events={data.getEvents} /> : <h2>No events yet.</h2>}
+    <div className="container dashboard">
+      <h1 className="title">{curDate}</h1>
+      <div className="plus-btn transition">
+        <div className="label transition">
+          <p >New Event</p>
+        </div>
+        <div className="btn-container transition">
+          <button className="btn" onClick={(e) => { history.push('/dashboard/add') }}><img src={require('../assets/plus.svg')} /></button>
+        </div>
+      </div>
+      {/* {testData ? <CardList events={testData.getEvents} /> : <h2 className='card-list'>No events yet.</h2>} */}
+      {data ? <CardList events={data.getEvents} /> : <h2 className='card-list'>No events yet.</h2>}
     </div>
   );
 };
-
