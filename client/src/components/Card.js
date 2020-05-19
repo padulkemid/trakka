0eff57b38c20accdbbf0d6603141258caa732d1b
Fadhil Muhammad <bringitdown0@gmail.com>
2020-05-19 14:33:23 +0700

    change testData to real data fetch from graphQl

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,26 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 export default ({ event }) => {
-    const [formattedTime, setFormattedTime] = useState('00:00')
+  const [formattedTime, setFormattedTime] = useState('00:00');
 
-    useEffect(() => {
-        console.log(event)
-        if (event.timestamp) {
-            setFormattedTime(getFormattedTime(new Date(event.timestamp)));
-        }
-    }, [event])
-
-    const getFormattedTime = (date) => {
-        const hour = date.getHours();
-        const minute = date.getMinutes();
-        return `${hour > 12 ? hour - 12 : hour}:${minute > 9 ? minute : `0${minute}`} ${hour > 12 ? 'pm' : 'am'}`;
+  useEffect(() => {
+    if (event.timestamp) {
+      const convertDate = Number(event.timestamp);
+      setFormattedTime(getFormattedTime(new Date(convertDate)));
     }
+  }, [event]);
 
-    return (
-        <div className='card'>
-            <h4 className='title'>{event.title}</h4>
-            <h5 className='date'>{formattedTime}</h5>
-            <p className='desc'>{event.description}</p>
-        </div>
-    )
-}
+  const getFormattedTime = (date) => {
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    return `${hour > 12 ? hour - 12 : hour}:${
+      minute > 9 ? minute : `0${minute}`
+    } ${hour > 12 ? 'pm' : 'am'}`;
+  };
+
+  return (
+    <div className="card">
+      <h4 className="title">{event.title}</h4>
+      <h5 className="date">{formattedTime}</h5>
+      <p className="desc">{event.description}</p>
+    </div>
+  );
+};
+
